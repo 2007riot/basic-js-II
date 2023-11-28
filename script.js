0f0563baa954123889705e418bbec87afb7395ee
2007riot <2007riot@gmail.com>
2023-11-28 10:37:45 +0100

    Completed exercise 2

--- script.js
+++ script.js
@@ -10,50 +10,84 @@ console.log('Hola soy tu consola y juntas vamos a aprender Javascript')
 
 //Ejercicio 1: Escribe un condicional if/else que imprima en la consola 'Eres mayor de edad' cuando la constante age tenga el valor correspondiente, si no es así, debe imprimir 'No eres aún mayor de edad'
 
-const age = 18
+let age = 18
 //Escribe tu código aquí
 
+function ageCheck(age) {
+    if (age >= 18) {
+        console.log('Eres mayor de edad')
+    } else {
+        console.log('No eres aún mayor de edad')
+    }
+}
+
+ageCheck(age)
 
 //Ejercicio 2: Cambia el valor de age a 12 para mirar el resultado del ejercicio anterior.
+age = 12
+ageCheck(age)
 
 
 //Ejercicio 3: Escribe un condicional if/else que lea la constante pet y si es un perro, que imprima en la consola "Tengo un perro", si es un gato, que imprima en la consola "Tengo un gato", si es un pájaro, que imprima en la consola "Tengo un pájaro" y si no es ninguno de los 3 que imprima "No tengo una mascota convencional"
 
 const pet = "perro";
 //Escribe tu código aquí
+function myPetIs(pet) {
+    switch (pet) {
+        case "perro":
+            console.log("Tengo un " + pet);
+            break;
+        case "gato":
+            console.log("Tengo un " + pet);
+            break;
+        case "pajaro":
+            console.log("Tengo un " + pet);
+            break;
+        default:
+            console.log("No tengo una mascota convencional")
+    }
+}
 
+myPetIs(pet)
 
 //Ejercicio 4: cambia el valor de pet a "serpiente" y mira el resultado en consola para comprobar que tu código funciona bien.
 
+myPetIs("serpiente")
 
 //Ejercicio 5: Haz el mismo ejercicio 3, pero con el condicional switch.
 
 //Escribe tu código aquí
 
+//ya he hecho con switch el ejercicio anterior
 
 //Ejercicio 6: Usando un operador ternario, escribe un programa que lea la constante weather e imprima en consola si es soleado "Me vestiré con un vestido" y si no es así que imprima "Me vestiré con pantalón"
 
-const weather = "soleado"
+let weather = "soleado";
 
-const result = //Escribe tu código aquí;
+//Escribe tu código aquí;
+const result = weather === "soleado" ? "Me vestiré con un vestido" : "Me vestiré con pantalón"
 
-console.log(result)
+console.log(result);
 
 //Ejercicio 7: cambia el valor de weather a "frío" y mira el resultado en consola para comprobar que tu código funciona bien.
+
+weather = "frio"
+console.log(result)
 
 //Ejercicio 8: Escribe un programa que al darle la constante 'value' imprima en consola "Es un número" cuando el valor sea de tipo number, "Es un string" cuando el valor sea de tipo string o si no es ni uno ni otro que imprima "No es ni número ni string". Puedes hacerlo con cualquier tipo de condicional.
 
 const value = '1'
-
 //Escribe tu código aquí
-
+console.log(value.isInteger ? "Es un numero" : typeof "string" ? "Es un string" : "Ni numero ni string")
 
 //BUCLES
 
 //Ejercicio 9: Imprime en consola una lista del 0 al 10 con el bucle for
-
 //Escribe tu código aquí
 
+for (let i = 0; i <= 10; i++) {
+    console.log(i);
+}
 
 //Ejercicio 10: Con un bucle for in imprime en pantalla todos los nombres, apellidos y su aporte a la sociedad de las programadoras de la historia contenidas en el array llamado 'programmers', Deberás imprimir el índice y la información de cada una de ellas de la siguiente manera: '0: Ada Lovelace, su aporte fue la máquina analítica'. 
 
@@ -77,24 +111,41 @@ const programmers = [
         name: 'Hedy',
         lastname: 'Lamarr',
         knowledge: 'el wifi'
-    }     
+    }
 ]
 
 //Escribe tu código aquí
+for (i in programmers) {
+    let programmer = programmers[i] 
+    output = `${i}` +": " + `${programmer.name}` + " " + `${programmer.lastname}` + ", su aporte fue " + `${programmer.knowledge}`
 
+    console.log(output)
+}
 
 //Ejercicio 11: Con un bucle for of imprime en consola "Hola, mi nombre es ... " y reemplaza los tres puntos con cada nombre del array dado.
 
 const names = ['Rocío', 'Lola', 'Antía', 'Laura', 'Noelia']
 
 //Escribe tu código aquí
-
+for (const i of names) {
+    console.log('Hola, mi nombre es ' + i)
+}
 
 //Ejercicio 12: Con un bucle while imprime en consola una lista del 1 al 5.
 
 //Escribe tu código aquí
 
+let z = 0
+// while (z< 5) {
+//     z ++
+//     console.log('while loop ' + z)
+// }
 
 //Ejercicio 13: Realiza el mismo ejercicio anterior pero con un bucle do while.
 
 //Escribe tu código aquí
+console.log("do while:")
+do {
+    z ++;
+    console.log(z);
+} while (z<5) 
